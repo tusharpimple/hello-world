0634bfb8f143202e7c60d2777db89672f5836f52
tusharpimple <38343045+tusharpimple@users.noreply.github.com>
2018-04-30 15:19:18 +0530

    Update int.java

--- int.java
+++ int.java
@@ -1,5 +1,5 @@
-class First {
-  public static void main(String[] arguments) {
-    System.out.println("Let's do something using Java technology.");
+class Simple{
+  public static void main(String args[]){
+    System.out.println("Hello Java");
   }
 }
